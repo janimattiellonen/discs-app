7655a07b89a87c83554cd5ca400baa15434d5011
janimattiellonen <janimatti.ellonen@gmail.com>
2021-03-14 15:07:15 +0200

    dd

--- api/disc.js
+++ api/disc.js
@@ -9,25 +9,16 @@ export default {
     const queryString = createQueryString({ query, filter, limit, offset, order })
 
     return axios
-      .get(`${config.server.base_url}/rest/discs?metafields=true&apikey=${config.server.api_key}&${queryString}`)
-      .then((res) => {
-        return res.data
-      })
-  },
-
-  getFoo() {
-    // // q={"name": {"$regex": "^mako3"}}&h={}&totals=true&count=true
-    const queryString = 'q={"name": {"$regex": "^mako3"}}&h={}&totals=true&count=true'
-
-    return axios
-      .get(`${config.server.base_url}/rest/discs?metafields=true&apikey=${config.server.api_key}&${queryString}`)
+      .get(
+        `${config.server.base_url}/rest/discs?metafields=true&apikey=${process.env.REACT_APP_API_KEY}&${queryString}`,
+      )
       .then((res) => {
         return res.data
       })
   },
 
   getStats() {
-    return axios.get(`${config.server.rest_base_url}/stats`).then((res) => {
+    return axios.get(`${process.env.REACT_APP_STATS_BASE_URL}/stats`).then((res) => {
       return res.data
     })
   },
